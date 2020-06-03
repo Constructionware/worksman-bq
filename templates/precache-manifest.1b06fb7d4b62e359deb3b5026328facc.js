563f099946c555cc2dac76278ca8491a9a530877
CentryPlan <csware.ware@gmail.com>
2020-06-03 18:35:42 -0500

    new file:   templates/favicon.ico 	new file:   templates/img/icons/android-chrome-192x192.png 	new file:   templates/img/icons/android-chrome-512x512.png 	new file:   templates/img/icons/apple-touch-icon.png 	new file:   templates/img/icons/favicon-16x16.png 	new file:   templates/img/icons/favicon-194x194.png 	new file:   templates/img/icons/favicon-32x32.png 	new file:   templates/img/icons/favicon-48x48.png 	new file:   templates/img/icons/favicon.ico 	new file:   templates/img/icons/favicon.svg 	new file:   templates/index.html 	new file:   templates/manifest.json 	new file:   templates/precache-manifest.1b06fb7d4b62e359deb3b5026328facc.js 	new file:   templates/robots.txt 	new file:   templates/service-worker.js 	new file:   templates/static/css/app.67f75590.css 	new file:   templates/static/css/chunk-461c3a3d.0ee5e45e.css 	new file:   templates/static/css/chunk-b04c980c.32f61650.css 	new file:   templates/static/img/couchdb.bc089528.png 	new file:   templates/static/img/favicon.47cb3ffd.svg 	new file:   templates/static/img/favicon.681c3e6a.svg 	new file:   templates/static/img/pouch.b694bdc7.png 	new file:   templates/static/img/tabulator_small.c5d2c0c8.png 	new file:   templates/static/img/user-white.7dab217e.svg 	new file:   templates/static/js/app.96fb033c.js 	new file:   templates/static/js/app.96fb033c.js.map 	new file:   templates/static/js/chunk-25db9a96.c22bd1c9.js 	new file:   templates/static/js/chunk-25db9a96.c22bd1c9.js.map 	new file:   templates/static/js/chunk-2d0bb1fd.e6f38671.js 	new file:   templates/static/js/chunk-2d0bb1fd.e6f38671.js.map 	new file:   templates/static/js/chunk-2d0e5041.0237ab83.js 	new file:   templates/static/js/chunk-2d0e5041.0237ab83.js.map 	new file:   templates/static/js/chunk-2d231585.b636b5d7.js 	new file:   templates/static/js/chunk-2d231585.b636b5d7.js.map 	new file:   templates/static/js/chunk-461c3a3d.3d4cfee7.js 	new file:   templates/static/js/chunk-461c3a3d.3d4cfee7.js.map 	new file:   templates/static/js/chunk-96e33dc0.19b04d03.js 	new file:   templates/static/js/chunk-96e33dc0.19b04d03.js.map 	new file:   templates/static/js/chunk-b04c980c.51e81603.js 	new file:   templates/static/js/chunk-b04c980c.51e81603.js.map 	new file:   templates/static/js/chunk-vendors.b0ef51f7.js 	new file:   templates/static/js/chunk-vendors.b0ef51f7.js.map

---
+++ templates/precache-manifest.1b06fb7d4b62e359deb3b5026328facc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c0cec3ae2e1c23d7d69d69119b72d7",
+    "url": "/qs/index.html"
+  },
+  {
+    "revision": "9366225d0f4afba18585956ee416f5d5",
+    "url": "/qs/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/qs/robots.txt"
+  },
+  {
+    "revision": "54b58229de6f7cd82e6b",
+    "url": "/qs/static/css/app.67f75590.css"
+  },
+  {
+    "revision": "25769273f4a2d97bb7a8",
+    "url": "/qs/static/css/chunk-461c3a3d.0ee5e45e.css"
+  },
+  {
+    "revision": "629e4745bae72f23fafd",
+    "url": "/qs/static/css/chunk-b04c980c.32f61650.css"
+  },
+  {
+    "revision": "bc08952889a8eeb908eeb28bbc1d5769",
+    "url": "/qs/static/img/couchdb.bc089528.png"
+  },
+  {
+    "revision": "47cb3ffd16cb94028c041f4c38d6bac0",
+    "url": "/qs/static/img/favicon.47cb3ffd.svg"
+  },
+  {
+    "revision": "681c3e6a254f977de79b1154f6864e05",
+    "url": "/qs/static/img/favicon.681c3e6a.svg"
+  },
+  {
+    "revision": "b694bdc7410b0ba4801185fea29701e5",
+    "url": "/qs/static/img/pouch.b694bdc7.png"
+  },
+  {
+    "revision": "c5d2c0c81fb698b1eca56ad608e14e75",
+    "url": "/qs/static/img/tabulator_small.c5d2c0c8.png"
+  },
+  {
+    "revision": "7dab217ef0edb74466854f49a5e7aebd",
+    "url": "/qs/static/img/user-white.7dab217e.svg"
+  },
+  {
+    "revision": "54b58229de6f7cd82e6b",
+    "url": "/qs/static/js/app.96fb033c.js"
+  },
+  {
+    "revision": "d6aa428acfff59d39845",
+    "url": "/qs/static/js/chunk-25db9a96.c22bd1c9.js"
+  },
+  {
+    "revision": "bcf68471081820620da7",
+    "url": "/qs/static/js/chunk-2d0bb1fd.e6f38671.js"
+  },
+  {
+    "revision": "94c42a0b36b95efc69bb",
+    "url": "/qs/static/js/chunk-2d0e5041.0237ab83.js"
+  },
+  {
+    "revision": "f5d6fe394a87b198d074",
+    "url": "/qs/static/js/chunk-2d231585.b636b5d7.js"
+  },
+  {
+    "revision": "25769273f4a2d97bb7a8",
+    "url": "/qs/static/js/chunk-461c3a3d.3d4cfee7.js"
+  },
+  {
+    "revision": "a9bc6c51ce702c6f0d26",
+    "url": "/qs/static/js/chunk-96e33dc0.19b04d03.js"
+  },
+  {
+    "revision": "629e4745bae72f23fafd",
+    "url": "/qs/static/js/chunk-b04c980c.51e81603.js"
+  },
+  {
+    "revision": "21c43cab281030d04b53",
+    "url": "/qs/static/js/chunk-vendors.b0ef51f7.js"
+  }
+]);
